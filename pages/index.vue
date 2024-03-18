3b45db44fd66c38679625bd08323ffab365accba
Luis Silva <l.silva06@ufromail.cl>
2024-03-18 20:18:47 -0300

    fix: change url avatar

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="px-5 w-full">
       <div class="grid md:grid-cols-12 gap-5 w-full items-center justify-items-center">
         <div class="max-w-md text-center col-span-4">
-          <img src="/img/luis-silva.jpeg" alt="avatar" class="rounded-full w-[30dvh] h-[30dvh] mx-auto" />
+          <img :src="urlAvatar.href" alt="avatar" class="rounded-full w-[30dvh] h-[30dvh] mx-auto" />
           <h1 class="text-5xl font-bold">{{ t('pages.index.title') }}</h1>
           <p class="py-6">{{ t('pages.index.description') }}</p>
           <button class="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg" @click="redirectToEmail">
@@ -25,6 +25,8 @@ definePageMeta({
 })
 
 const { t } = useI18n()
+
+const urlAvatar = new URL("https://media.licdn.com/dms/image/D4E03AQFZ35-GIWO2Hg/profile-displayphoto-shrink_800_800/0/1699043197238?e=1716422400&v=beta&t=LT0a2h1A5LHWf8cEWsUjHcxMPCsgemuLrZKUxQeXpsY")
 
 const redirectToEmail = () => {
   const url = new URL("mailto:luishernansilvaq@gmail.cl?subject=Contacto&body=Hola Luis, ...");
