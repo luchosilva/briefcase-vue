73441c0b4991fb3520ef45444922d56ffeb66e13
Luis Silva <l.silva06@ufromail.cl>
2024-03-18 20:21:30 -0300

    fix: test i18n

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,11 @@
       <div class="grid md:grid-cols-12 gap-5 w-full items-center justify-items-center">
         <div class="max-w-md text-center col-span-4">
           <img :src="urlAvatar.href" alt="avatar" class="rounded-full w-[30dvh] h-[30dvh] mx-auto" />
-          <h1 class="text-5xl font-bold">{{ t('pages.index.title') }}</h1>
+          <!-- <h1 class="text-5xl font-bold">{{ t('pages.index.title') }}</h1>
           <p class="py-6">{{ t('pages.index.description') }}</p>
           <button class="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg" @click="redirectToEmail">
             {{ t('pages.index.contact-by-email') }}
-          </button>
+          </button> -->
         </div>
         <div class="col-span-8 max-h-[80dvh] overflow-auto scroll-m-1">
           <!-- <projects /> -->
