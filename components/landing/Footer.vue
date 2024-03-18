333bef8fd4871a04c8928509473e99e5748ebfbe
Luis Silva <l.silva06@ufromail.cl>
2024-03-18 19:06:16 -0300

    fix: enhancements footer links

--- components/landing/Footer.vue
+++ components/landing/Footer.vue
@@ -2,15 +2,23 @@
   <div>
     <footer class="footer items-center p-4 md:container md:mx-auto">
       <div class="items-center grid-flow-col">
-        <a href="https://github.com/luchosilva" target="_blank">
+        <a :href="githubUrl.href" target="_blank">
           <Icon name="fa-brands:github" size="2.5em" />
         </a>
       </div>
       <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a href="https://www.linkedin.com/in/luis-silva-ufro-carahue/" target="_blank">
+        <a :href="linkedinUrl.href" target="_blank">
           <Icon name="fa-brands:linkedin" size="2.5em" />
         </a>
       </div>
     </footer>
   </div>
 </template>
+
+<script setup lang="ts">
+
+const githubUrl = new URL('https://github.com/luchosilva')
+
+const linkedinUrl = new URL('https://www.linkedin.com/in/luis-silva-ufro-carahue')
+
+</script>
