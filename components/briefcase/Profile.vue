e87054d37edf4ed092055f8e47aa30cad227da7d
Luis Silva <l.silva06@ufromail.cl>
2024-03-22 12:48:26 -0300

    fix: url mail

--- components/briefcase/Profile.vue
+++ components/briefcase/Profile.vue
@@ -8,7 +8,7 @@
     <h1 class="text-5xl font-bold">{{ t("pages.index.title") }}</h1>
     <p class="py-6">{{ t("pages.index.description") }}</p>
     <a
-      :href="urlAvatar.href"
+      :href="urlMail.href"
       target="_blank"
       class="btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg"
     >
@@ -23,4 +23,9 @@ const { t } = useI18n();
 const urlAvatar = new URL(
   "https://media.licdn.com/dms/image/D4E03AQFZ35-GIWO2Hg/profile-displayphoto-shrink_800_800/0/1699043197238?e=1716422400&v=beta&t=LT0a2h1A5LHWf8cEWsUjHcxMPCsgemuLrZKUxQeXpsY"
 );
+
+const urlMail = new URL(
+  "mailto:luishernansilvaq@gmail.cl?subject=Contacto&body=Hola Luis, ..."
+);
+
 </script>
