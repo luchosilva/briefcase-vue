ed570fd52a1804adb612b657db4a2467abd9dd3d
Luis Silva <l.silva06@ufromail.cl>
2024-03-22 13:26:27 -0300

    feat: add loading

--- layouts/landing.vue
+++ layouts/landing.vue
@@ -9,8 +9,11 @@
         <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content flex flex-col">
           <slot name="header">
-            <client-only fallback-tag="span" fallback="Loading on server...">
+            <client-only fallback-tag="span">
               <landing-navbar />
+              <template #fallback>
+                <loading />
+              </template>
             </client-only>
           </slot>
           <div class="flex-1 w-full flex flex-col">
