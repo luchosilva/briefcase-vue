6459b6ce92b986ffaa57b39bfcb8e9123aec5215
Luis Silva <l.silva06@ufromail.cl>
2024-04-06 15:12:57 -0300

    feat: enhancements timeline

--- components/briefcase/Timeline.vue
+++ components/briefcase/Timeline.vue
@@ -1,91 +1,62 @@
 <template>
-    <div>
-        <ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-            <li>
-                <div class="timeline-middle">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                        <path fill-rule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </div>
-                <div class="timeline-start md:text-end mb-10">
-                    <time class="font-mono italic">
-                        {{ t('pages.index.projects[0].range') }}
-                    </time>
-                    <div class="text-lg font-black">
-                        {{ t('pages.index.projects[0].name') }}
-                    </div>
-                    {{ t('pages.index.projects[0].description') }}
-                </div>
-                <hr class="bg-primary" />
-            </li>
-            <li>
-                <hr class="bg-primary" />
-                <div class="timeline-middle">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                        <path fill-rule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </div>
-                <div class="timeline-end mb-10">
-                    <time class="font-mono italic">
-                        {{ t('pages.index.projects[1].range') }}
-                    </time>
-                    <div class="text-lg font-black">
-                        {{ t('pages.index.projects[1].name') }}
-                    </div>
-                    {{ t('pages.index.projects[1].description') }}
-                </div>
-                <hr class="bg-primary" />
-            </li>
-            <li>
-                <hr class="bg-primary" />
-                <div class="timeline-middle">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                        <path fill-rule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                            clip-rule="evenodd" />
-
-                    </svg>
-                </div>
-                <div class="timeline-start md:text-end mb-10">
-                    <time class="font-mono italic">
-                        {{ t('pages.index.projects[2].range') }}
-                    </time>
-                    <div class="text-lg font-black">
-                        {{ t('pages.index.projects[2].name') }}
-                    </div>
-                    {{ t('pages.index.projects[2].description') }}
-                </div>
-                <hr class="bg-primary" />
-            </li>
-            <li>
-                <hr class="bg-primary" />
-                <div class="timeline-middle">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
-                        <path fill-rule="evenodd"
-                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </div>
-                <div class="timeline-end mb-10">
-                    <time class="font-mono italic">
-                        {{ t('pages.index.projects[3].range') }}
-                    </time>
-                    <div class="text-lg font-black">
-                        {{ t('pages.index.projects[3].name') }}
-                    </div>
-                    {{ t('pages.index.projects[3].description') }}
-                </div>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <ul
+      class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical"
+    >
+      <li v-for="(project, index) in projects" :key="project.name">
+        <div class="timeline-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="h-5 w-5"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <div
+          class="mb-10"
+          :class="{
+            'timeline-start md:text-end': isParNumber(index),
+            'timeline-end md:text-start': !isParNumber(index),
+          }"
+        >
+          <time class="text-sm md:text-base font-mono italic">
+            {{ rt(project.range) }}
+          </time>
+          <div class="text-base md:text-lg font-black">
+            {{ rt(project.name) }}
+          </div>
+          <p
+            v-for="el in rt(project.description)?.split('\n')"
+            :key="el.slice(0, 5)"
+            class="text-sm md:text-base"
+          >
+            {{ el }}
+          </p>
+        </div>
+        <hr v-if="index !== projects.length - 1" class="bg-primary" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
+const { tm, rt } = useI18n();
 
-const { t } = useI18n()
+type Project = {
+  name: string;
+  range: string;
+  description: string;
+};
 
+const isParNumber = (num: number) => num % 2 === 0;
+
+const projects: ComputedRef<Project[]> = computed(() => {
+  return tm("pages.index.projects");
+});
 </script>
